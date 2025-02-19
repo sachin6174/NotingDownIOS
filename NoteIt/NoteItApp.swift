30d36f8368357e8b3c6aa6eb7f1b0deacc8fdf36
sachin kumar <sachinmehtab@gmail.com>
2025-02-19 14:32:15 +0530

    Initial Commit

---
+++ NoteIt/NoteItApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteItApp.swift
+//  NoteIt
+//
+//  Created by sachin kumar on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
